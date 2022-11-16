360589983c16ef645e95906c884cd229ccbde4ca
bernardo Cunha Capoferri <bernardocc@al.insper.edu.br>
2022-11-16 15:29:04 -0300

    começando o python

---
+++ python/projeto.py
@@ -0,0 +1,17 @@
+import os
+import json
+
+def read_json():
+    return None
+
+def write_json():
+    return None
+
+def convert_class_to_json():
+    return None
+
+def main():
+    dic_instancias = {}
+    dic_user = {}
+    region = "us-east-1"
+    
